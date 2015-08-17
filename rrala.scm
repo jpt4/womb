e75d90038a67b8a79ab420c39dc8bd0a49df8bcc
James P. Torre, IV <torreaiworks01@gmail.com>
2015-08-17 04:06:35 +0000

    Report and change rrala object state.

--- rrala.scm
+++ rrala.scm
@@ -1,16 +1,44 @@
 ;;  rrala.scm  jpt4  20150814
 
 (define (rrala-cell id)
-	(let act ([role 000]
-						[recv '(0 0 0 0 0 0)]
-						[send '(0 0 0 0 0 0)]
-						[pals '(x x x x x x)])
-		(lambda (msg)
-			(case msg
-				[000 (lambda () role)]
-				[001 (lambda (val) (act val recv send pals))]
-				[else 'exit]
-				))))
+	(let* ([role 000]  ;; stem gate pass font sink
+						[recv '(0 0 0 0 0 0)]  ;;  left to right, top to bottom
+						[send '(0 0 0 0 0 0)]  ;;  left to right, top to bottom
+						[pals '(x x x x x x)]  ;;  left to right, top to bottom
+						[state (list id role recv send pals)]
+						[report (lambda (stat)
+											(case stat
+												['id id]
+												['role role]
+												['recv recv]
+												['send send]
+												['pals pals]
+												['state state]
+												[else stat]
+												))
+										]
+						[change (lambda (prev curr)
+											(case prev
+												['id (set! id curr)]
+												['role (set! role curr)]
+												['recv (set! recv curr)]
+												['send (set! send curr)]
+												['pals (set! pals curr)]))]
+						[act (lambda ()
+									 (cond
+										[(eq? role 'gate) 
+						)
+					 (lambda (msg)
+						 (case msg
+							 ;;  report state
+							 [? (lambda (diag) (report diag))]
+							 ;;  change state
+							 [! (lambda (old new) (change old new))]
+							 ;;  
+							 [else 'exit]
+							 )
+						 
+						 )))
 
 (define rr0 (rrala-cell 0))
 					
