3612265f2c95d83ad1fcbae14ca0890bd1e0f87f
jpt4-crux <torreaiworks01@gmail.com>
2020-10-24 06:07:01 +0000

    Prime ratio calculations.

--- cljexp/collatz/src/collatz/core.clj
+++ cljexp/collatz/src/collatz/core.clj
@@ -72,6 +72,37 @@
 (defn branch-seeds [t] 
   (reduce (fn [ls x] (concat ls (list (first (last x))))) '() t))
 
+(defn is-prime? [n]
+  (and (not (= 0 n)) (not (= 1 n))
+       (not (reduce (fn [bc i] (or bc (= 0 (mod n (+ 1 i))))) 
+                    false 
+                    (range 1 (Math/floor (Math/sqrt n)))))))
+
+;t <- tree
+(defn prime-count [t]
+  (count (filter is-prime? (branch-seeds t))))
+
+(defn prime-ratio [t]
+  (let [tree-size (size-of-ctree t)
+        seeds (branch-seeds t)
+        seed-count (count seeds)
+        primes (filter is-prime? seeds)
+        prime-count (count primes)
+        primes-to-seeds (/ prime-count seed-count)
+        primes-to-tree (/ prime-count tree-size)]
+    (println
+     'tree-size tree-size
+     'seed-count seed-count
+     'prime-count prime-count
+     'prime-count-to-seed-count-ratio primes-to-seeds
+     'prime-count-to-tree-size-ratio primes-to-tree)))
+
+(defn expected-prime-count [n] (/ n (Math/log n)))
+(defn expected-prime-ratio [n] (/ 1 (Math/log n)))
+(defn primes-less-than [n] (count (filter is-prime? (range 1 n))))
+(defn ctree-to-numberline-prime-ratio [n]
+  (* 1.0 (/ (prime-count (ctree n)) (primes-less-than n))))
+
 (defn foo
   "I don't do a whole lot."
   [x]
